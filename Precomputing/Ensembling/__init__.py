b42f93b38467ef08353343c9154184ee7c68201f
Keyu Duan <104810828@qq.com>
2022-10-29 14:33:36 +0800

    code clean

--- Precomputing/Ensembling/__init__.py
+++ Precomputing/Ensembling/__init__.py
@@ -1,9 +1,5 @@
 from .AdaGCN import AdaGCN
-from .AdaGCN_CandS import AdaGCN_CandS
-from .GBGCN import GBGCN
-from .SAdaGCN import SAdaGCN
-from .AdaGCN_SLE import AdaGCN_SLE
 from .EnGCN import EnGCN
 from .WeakLearners import *
 
-__all__ = ["AdaGCN", "GBGCN", "SAdaGCN", "AdaGCN_CandS", "AdaGCN_SLE", "EnGCN"]
+__all__ = ["AdaGCN", "EnGCN"]
