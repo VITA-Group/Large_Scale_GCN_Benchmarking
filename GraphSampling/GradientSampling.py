fix label leakage issue, rerun the experiments

GraphSampling/GradientSampling.py
@@ -1,5 +1,4 @@
 import math
-import seaborn as sns
 import os
 from typing import Callable, Optional
 
@@ -85,9 +84,7 @@ class GradientSampler:
         row, col = data.edge_index[0], data.edge_index[1]
         edge_attr = torch.ones(row.size(0), requires_grad=True).to(data.x.device)
         # edge_attr = torch.range(0, self.E, dtype=torch.long, requires_grad=True).to(data.x.device)
-        self.adj = SparseTensor(
-            row=row, col=col, value=edge_attr, sparse_sizes=(self.N, self.N)
-        )
+        self.adj = SparseTensor(row=row, col=col, value=edge_attr, sparse_sizes=(self.N, self.N))
 
     def __reset_params__(self):
         raise NotImplementedError
@@ -203,9 +200,7 @@ class GradientSampling(_GraphSampling):
                 out = F.log_softmax(out, dim=-1)
                 loss = loss_op(out[batch.train_mask], batch.y[batch.train_mask])
             else:
-                loss = loss_op(
-                    out[batch.train_mask], batch.y[batch.train_mask].type_as(out)
-                )
+                loss = loss_op(out[batch.train_mask], batch.y[batch.train_mask].type_as(out))
 
             weight = 1.0 / self.train_loader.prob[batch.node_idx[batch.train_mask]]
             weight = weight.to(device)
@@ -231,11 +226,7 @@ class GradientSampling(_GraphSampling):
                 total_correct += int(out.argmax(dim=-1).eq(batch.y).sum())
             else:
                 total_correct += int(out.eq(batch.y).sum())
-        train_size = (
-            self.train_size
-            if isinstance(loss_op, torch.nn.NLLLoss)
-            else self.train_size * self.num_classes
-        )
+        train_size = self.train_size if isinstance(loss_op, torch.nn.NLLLoss) else self.train_size * self.num_classes
 
         # cosine annealing
         lr = (self.update_rate / 2) * (1 + math.cos(epoch * math.pi / self.T_end))
@@ -251,7 +242,7 @@ class GradientSampling(_GraphSampling):
         #     plot = sns.histplot(self.train_loader.grads)
         #     fig = plot.get_figure()
         #     fig.savefig("./figs/grads_%d.png" % epoch)
-            # plot.clf()
+        # plot.clf()
 
         return total_loss / self.num_steps, total_correct / train_size, self.train_loader.grads
 

Precomputing/Ensembling/WeakLearners.py
@@ -24,9 +24,7 @@ class MLP(torch.nn.Module):
             self.batch_norm_list.append(BatchNorm1d(args.dim_hidden))
         self.linear_list.append(Linear(args.dim_hidden, args.num_classes))
 
-        self.optimizer = torch.optim.Adam(
-            self.parameters(), lr=args.lr, weight_decay=args.weight_decay
-        )
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=args.lr, weight_decay=args.weight_decay)
         self.loss_op = torch.nn.NLLLoss()
 
     def forward(self, x):
@@ -99,9 +97,7 @@ class MLP_SLE(torch.nn.Module):
                 args.dropout,
                 normalization="batch" if args.use_batch_norm else "none",
             )
-        self.optimizer = torch.optim.Adam(
-            self.parameters(), lr=args.lr, weight_decay=args.weight_decay
-        )
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=args.lr, weight_decay=args.weight_decay)
         self.loss_op = torch.nn.NLLLoss()
 
     def forward(self, x, y, use_label_mlp):
@@ -111,12 +107,8 @@ class MLP_SLE(torch.nn.Module):
         return out
 
     def load(self):
-        self.base_mlp.load_state_dict(
-            torch.load(f"./.cache/{self.type_model}_{self.dataset}_base_mlp.pt")
-        )
-        self.label_mlp.load_state_dict(
-            torch.load(f"./.cache/{self.type_model}_{self.dataset}_label_mlp.pt")
-        )
+        self.base_mlp.load_state_dict(torch.load(f"./.cache/{self.type_model}_{self.dataset}_base_mlp.pt"))
+        self.label_mlp.load_state_dict(torch.load(f"./.cache/{self.type_model}_{self.dataset}_label_mlp.pt"))
 
     def train_net(self, train_loader, loss_op, device, use_label_mlp):
         self.train()
@@ -159,9 +151,7 @@ class MLP_SLE(torch.nn.Module):
 
 
 class Inner_MLP(torch.nn.Module):
-    def __init__(
-        self, in_dim, hidden_dim, out_dim, num_layers, dropout, use_batch_norm
-    ):
+    def __init__(self, in_dim, hidden_dim, out_dim, num_layers, dropout, use_batch_norm):
         super(Inner_MLP, self).__init__()
         self.linear_list = torch.nn.ModuleList()
         self.batch_norm_list = torch.nn.ModuleList()
@@ -190,9 +180,7 @@ class Inner_MLP(torch.nn.Module):
 class MultiHeadLinear(nn.Module):
     def __init__(self, in_feats, out_feats, n_heads, bias=True):
         super().__init__()
-        self.weight = nn.Parameter(
-            torch.FloatTensor(size=(n_heads, in_feats, out_feats))
-        )
+        self.weight = nn.Parameter(torch.FloatTensor(size=(n_heads, in_feats, out_feats)))
         if bias:
             self.bias = nn.Parameter(torch.FloatTensor(size=(n_heads, 1, out_feats)))
         else:
@@ -227,9 +215,7 @@ class MultiHeadLinear(nn.Module):
 
 # Modified multi-head BatchNorm1d layer
 class MultiHeadBatchNorm(nn.Module):
-    def __init__(
-        self, n_heads, in_feats, momentum=0.1, affine=True, device=None, dtype=None
-    ):
+    def __init__(self, n_heads, in_feats, momentum=0.1, affine=True, device=None, dtype=None):
         factory_kwargs = {"device": device, "dtype": dtype}
         super().__init__()
         assert in_feats % n_heads == 0
@@ -244,12 +230,8 @@ class MultiHeadBatchNorm(nn.Module):
             self.register_parameter("weight", None)
             self.register_parameter("bias", None)
 
-        self.register_buffer(
-            "running_mean", torch.zeros(size=(n_heads, in_feats // n_heads))
-        )
-        self.register_buffer(
-            "running_var", torch.ones(size=(n_heads, in_feats // n_heads))
-        )
+        self.register_buffer("running_mean", torch.zeros(size=(n_heads, in_feats // n_heads)))
+        self.register_buffer("running_var", torch.ones(size=(n_heads, in_feats // n_heads)))
         self.running_mean: Optional[Tensor]
         self.running_var: Optional[Tensor]
         self.reset_parameters()
@@ -276,12 +258,8 @@ class MultiHeadBatchNorm(nn.Module):
             mean = x.mean(dim=0, keepdim=True)
             var = x.var(dim=0, unbiased=False, keepdim=True)
             out = (x - mean) * torch.rsqrt(var + eps)
-            self.running_mean = (
-                1 - self._momentum
-            ) * self.running_mean + self._momentum * mean.detach()
-            self.running_var = (
-                1 - self._momentum
-            ) * self.running_var + self._momentum * var.detach()
+            self.running_mean = (1 - self._momentum) * self.running_mean + self._momentum * mean.detach()
+            self.running_var = (1 - self._momentum) * self.running_var + self._momentum * var.detach()
         else:
             out = (x - self.running_mean) * torch.rsqrt(self.running_var + eps)
         if self._affine:
@@ -310,6 +288,7 @@ class GroupMLP(nn.Module):
         self._n_layers = n_layers
 
         self.input_drop = nn.Dropout(input_drop)
+        self.norm_type = normalization
 
         if self._n_layers == 1:
             self.layers.append(MultiHeadLinear(in_feats, out_feats, n_heads))
@@ -342,9 +321,7 @@ class GroupMLP(nn.Module):
 
                 nn.init.kaiming_uniform_(layer.weight[head], a=math.sqrt(5))
                 if layer.bias is not None:
-                    fan_in, _ = nn.init._calculate_fan_in_and_fan_out(
-                        layer.weight[head]
-                    )
+                    fan_in, _ = nn.init._calculate_fan_in_and_fan_out(layer.weight[head])
                     bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
                     nn.init.uniform_(layer.bias[head], -bound, bound)
         self.reset_parameters()
@@ -358,8 +335,9 @@ class GroupMLP(nn.Module):
                 nn.init.xavier_uniform_(layer.weight[head], gain=gain)
                 if layer.bias is not None:
                     nn.init.zeros_(layer.bias[head])
-        for norm in self.norms:
-            norm.reset_parameters()
+        if self.norm_type != "none":
+            for norm in self.norms:
+                norm.reset_parameters()
             # for norm in self.norms:
             #     norm.moving_mean[head].zero_()
             #     norm.moving_var[head].fill_(1)

main.py
@@ -53,9 +53,7 @@ def main(args):
             else:
                 t = os.path.getmtime(path_json)
                 tstr = datetime.fromtimestamp(t).strftime("%Y_%m_%d_%H_%M_%S")
-                os.rename(
-                    path_json, os.path.join(filedir, filename + "_" + tstr + ".json")
-                )
+                os.rename(path_json, os.path.join(filedir, filename + "_" + tstr + ".json"))
         if resume_seed >= args.N_exp:
             print("Training already finished!")
             return
@@ -96,9 +94,7 @@ def main(args):
 
         ## record training data
         print(
-            "mean and std of test acc: {:.4f} {:.4f} ".format(
-                np.mean(list_test_acc) * 100, np.std(list_test_acc) * 100
-            )
+            "mean and std of test acc: {:.4f} {:.4f} ".format(np.mean(list_test_acc) * 100, np.std(list_test_acc) * 100)
         )
 
         try:
@@ -118,6 +114,7 @@ def main(args):
         "final mean and std of test acc: ",
         f"{np.mean(list_test_acc)*100:.4f} $\\pm$ {np.std(list_test_acc)*100:.4f}",
     )
+    return np.mean(list_test_acc)
 
 
 if __name__ == "__main__":